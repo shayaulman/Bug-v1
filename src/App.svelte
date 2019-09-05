47f2d2dce9ac8f076c8ef51bd106b3470ac4f545
Shaye Ulman <shayeulman@gmail.com>
2019-09-05 20:52:03 +0300

    ✨initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let bugs = [false, true, false, false, true];
+
+	const debug = (index) => {
+		if (bugs[index] === true) return
+
+		bugs = bugs.map((bug, i) => bug === false && Math.random() > 0.5 && i !== index);
+	}
+</script>
+<div>
+	{#each bugs as bug, i}
+		<input type="checkbox" on:change={() => debug(i)} bind:checked={bug}>
+	<p>
+		{i}
+	</p>
+	{/each}
+</div>
