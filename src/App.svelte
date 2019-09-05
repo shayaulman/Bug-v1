62d6a4b3f5e61a7d3dcfa2e528a03a1b4182a885
Shaye Ulman <shayeulman@gmail.com>
2019-09-05 22:17:00 +0300

    Got things to work

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,27 @@
 <script>
 	let bugs = [false, true, false, false, true];
-
+	let solved = false;
 	const debug = (index) => {
-		if (bugs[index] === true) return
+		if (bugs[index] === true) return;
+		bugs = bugs.map((bug, i) => bug === true ? true : Math.random() > 0.50 && i !== index);
+		solved = !bugs.includes(true);
+	}
 
-		bugs = bugs.map((bug, i) => bug === false && Math.random() > 0.5 && i !== index);
+	let chance = [...Array(bugs.length).keys()].slice(1).reduce((a,b) => a*b);
+	const addBug = () => {
+		let a = [...bugs].push(true);
+		bugs = a;
 	}
 </script>
 <div>
+<p>Try to debug this...</p>
+<p>You have a  1 / { chance } chance to debug this (to undrstand why - see the code...)</p>
+{#if !solved }
 	{#each bugs as bug, i}
-		<input type="checkbox" on:change={() => debug(i)} bind:checked={bug}>
-	<p>
-		{i}
-	</p>
+		<input type="checkbox" on:change={ () => debug(i) } bind:checked={ bug }>
 	{/each}
+{:else}
+	<p>'Hooray!!'</p>
+{/if}
+<button on:click={ addBug }>+</button>
 </div>
